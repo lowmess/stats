9986775ee7f0496e6d0c115f66e04224067b5b2c
Alec Lomas <alec@lowmess.com>
2019-04-30 20:29:19 -0700

    :bug: properly count recent github commits
    this _massively_ simplifies the resolver. win-win #18

--- graphql/resolvers/commits.js
+++ graphql/resolvers/commits.js
@@ -2,49 +2,16 @@ const fetch = require('../lib/fetchWithTimeout')
 const { thirtyDaysAgo } = require('../lib/date')
 
 const getCommits = async () => {
-  const query = `query recentCommits($date: GitTimestamp, $author: CommitAuthor) {
+  const query = `query recentCommits($date: DateTime) {
     viewer {
-      repositories(first: 100) {
-        nodes {
-          ref(qualifiedName: "master") {
-            target {
-              ... on Commit {
-                history(since: $date, first: 100, author: $author) {
-                  edges {
-                    node {
-                      id
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-
-      repositoriesContributedTo(first: 100) {
-        nodes {
-          ref(qualifiedName: "master") {
-            target {
-              ... on Commit {
-                history(since: $date, first: 100, author: $author) {
-                  edges {
-                    node {
-                      id
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
+      contributionsCollection(from: $date) {
+        totalCommitContributions
       }
     }
   }`
 
   const variables = {
     date: thirtyDaysAgo().toISOString(),
-    author: { id: process.env.GITHUB_ID },
   }
 
   const options = {
@@ -66,29 +33,7 @@ const getCommits = async () => {
     throw new Error(`GitHub responded without a data object`)
   }
 
-  let amount = 0
-
-  data.viewer.repositories.nodes.forEach(node => {
-    if (node.ref) {
-      node.ref.target.history.edges.forEach(edge => {
-        if (edge.node.id) {
-          amount++
-        }
-      })
-    }
-  })
-
-  data.viewer.repositoriesContributedTo.nodes.forEach(node => {
-    if (node.ref) {
-      node.ref.target.history.edges.forEach(edge => {
-        if (edge.node.id) {
-          amount++
-        }
-      })
-    }
-  })
-
-  return amount
+  return data.viewer.contributionsCollection.totalCommitContributions
 }
 
 module.exports = getCommits
