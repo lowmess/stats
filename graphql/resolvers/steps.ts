85b664714abafc6c88430482df293d69a62a117b
Alec Lomas <alec@lowmess.com>
2020-02-14 19:13:00 -0700

    :bug: refresh withings token when service responds 401
    gross

--- graphql/resolvers/steps.ts
+++ graphql/resolvers/steps.ts
@@ -6,6 +6,32 @@ interface Activity {
   readonly steps: number
 }
 
+// This is probably not the best way to do this, since I'll have to re-generate
+// new tokens for each deploy. But I also don't want to run a server just to
+// generate & store these tokens.
+let accessToken = process.env.WITHINGS_KEY
+let refreshToken = process.env.WITHINGS_REFRESH_KEY
+
+const getNewToken = async (): Promise<void> => {
+  const uri = 'https://account.withings.com/oauth2/token'
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      grant_type: 'refresh_token',
+      client_id: process.env.WITHINGS_CLIENT_ID,
+      client_secret: process.env.WITHINGS_CLIENT_SECRET,
+      refresh_token: refreshToken,
+    }),
+  }
+
+  const response = await fetch(uri, options)
+  const data = await response.json()
+
+  accessToken = data.access_token
+  refreshToken = data.refresh_token // eslint-disable-line require-atomic-updates
+}
+
 const getSteps = async (): Promise<number> => {
   const uri = `https://wbsapi.withings.net/v2/measure?action=getactivity&startdateymd=${format(
     thirtyDaysAgo(),
@@ -14,12 +40,17 @@ const getSteps = async (): Promise<number> => {
 
   const options = {
     headers: {
-      Authorization: `Bearer ${process.env.WITHINGS_KEY}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   }
 
   const response = await fetch(uri, options)
   const data = await response.json()
+
+  if (data.status === 401) {
+    await getNewToken()
+    return getSteps()
+  }
 
   if (!data?.body?.activities) {
     throw new Error(`Withings did not provide steps data in their response`)
